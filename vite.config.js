57a8d92d8ba47cb9b81fd92f77e4964237f8d4f7
sundeep-kp <sundeep25101@iiitnr.edu.in>
2025-12-16 01:49:51 +0530

    removed references to learn.html from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,13 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        // Updated entry point for the LEARN page
-        learn: resolve(__dirname, 'learn.html'), 
+        
+        
         // Second entry point for the PLAYGROUND page
         playground: resolve(__dirname, 'public/playground.html'), 
       },
     },
   },
   // Ensure development server serves learn.html by default
-  server: {
-    open: '/learn.html'
-  }
+  
 });
