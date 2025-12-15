1090b776aaa797cd5219575045503d3c1b4a1158
sundeep-kp <sundeep25101@iiitnr.edu.in>
2025-12-16 02:43:58 +0530

    removed absolute path references because netlify hates it, moved playground.html to root

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
-        playground: '/public/playground.html'
+        playground: 'playground.html'
       }
     }
   }
